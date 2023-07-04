e95bbbe8c17ddba0623884d258d9bef67ac308a2
askar-cleancode <136423628+askar-cleancode@users.noreply.github.com>
2023-07-04 10:13:56 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -23,19 +23,26 @@ TEST_CASE("classifyTemperatureBreach according to limits too Low") {
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
 }
 
+TEST_CASE("classifyTemperatureBreach according to limits too high") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 42) == NORMAL);
+}
+
+TEST_CASE("classifyTemperatureBreach according to limits too Low") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42) == TOO_HIGH);
+}
 
 TEST_CASE("checkAndAlert to controller according to limits too high") {
   BatteryCharacter batteryChar = {PASSIVE_COOLING, "bosch"};
-  REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, 50) );
+  checkAndAlert(TO_CONTROLLER, batteryChar, 50) ;
 }
 
 TEST_CASE("checkAndAlert to mail according to limits too low") {
   BatteryCharacter batteryChar = {HI_ACTIVE_COOLING, "bosch"};
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, -1) );
+  checkAndAlert(TO_EMAIL, batteryChar, -1) ;
 }
 
 TEST_CASE("checkAndAlert to controller according to limits Normal") {
   BatteryCharacter batteryChar = {MED_ACTIVE_COOLING, "bosch"};
-  REQUIRE(checkAndAlert(TO_EMAIL, batteryChar, 20));
+  checkAndAlert(TO_EMAIL, batteryChar, 20);
 }
 
