add48a364a5a31b9282a9b0e71d836c4c4dc60ac
askar-cleancode <136423628+askar-cleancode@users.noreply.github.com>
2023-07-04 09:08:44 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -6,3 +6,11 @@
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
+
+TEST_CASE("infers the breach according to limits high") {
+  REQUIRE(inferBreach(35, 20, 30) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach according to limits") {
+  REQUIRE(inferBreach(25, 20, 30) == NORMAL);
+}
